3f6980f6b7993d5e67a75b15488cb55ec4f66e62
Matthias Hansen <matthias.hansen@rwth-aachen.de>
2017-04-11 17:27:40 +0200

    pull contition further up

--- src/main/scala/com/gildedrose/GildedRose.scala
+++ src/main/scala/com/gildedrose/GildedRose.scala
@@ -21,7 +21,7 @@ class GildedRose(val items: Array[Item]) {
       else if (isAgedBrie(item)) qualityChangeAgedBrie(item)
       else if (isBackstagePasses(item)) qualityChangeBackstagePasses(item)
       else qualityChangeNormalItem(item)
-    newQualityWithinTheBounds(item.quality,qualityIncrease)
+    if(qualityIncrease == 0) item.quality else newQualityWithinTheBounds(item.quality,qualityIncrease)
   }
 
   private def qualityChangeBackstagePasses(item: Item) =
@@ -34,8 +34,7 @@ class GildedRose(val items: Array[Item]) {
   private def qualityChangeAgedBrie(item: Item) = -qualityChangeNormalItem(item)
   private def qualityChangeSulfuras(item:Item) = 0
 
-  private def newQualityWithinTheBounds(currentQuality:Int, qualityIncrease: Int) =
-    if (qualityIncrease != 0) Math.min(Math.max(currentQuality + qualityIncrease, 0), 50) else currentQuality
+  private def newQualityWithinTheBounds(currentQuality:Int, qualityIncrease: Int) = Math.min(Math.max(currentQuality + qualityIncrease, 0), 50)
 
   private def hasToBeSold(i: Item) = !isSulfuras(i)
 }
