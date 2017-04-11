bac9182e12314540dd3ca1ce91b2a9e89b6b8af9
Matthias Hansen <matthias.hansen@rwth-aachen.de>
2017-04-11 16:43:28 +0200

    rename parameter

--- src/main/scala/com/gildedrose/GildedRose.scala
+++ src/main/scala/com/gildedrose/GildedRose.scala
@@ -22,20 +22,20 @@ class GildedRose(val items: Array[Item]) {
     }
   }
 
-  private def determineNewQuality(getItem: Item) = {
+  private def determineNewQuality(item: Item) = {
 
-    if (shouldJustLoseQuality(getItem)) {
-      val qualityLoss = if (getItem.sellIn <= 0) 2 else 1
-      getItem.quality = Math.max(getItem.quality - qualityLoss, 0)
+    if (shouldJustLoseQuality(item)) {
+      val qualityLoss = if (item.sellIn <= 0) 2 else 1
+      item.quality = Math.max(item.quality - qualityLoss, 0)
     }
-    if (isAgedBrie(getItem)) {
-      val qualityIncrease = if (getItem.sellIn <= 0) 2 else 1
-      getItem.quality = Math.min(getItem.quality + qualityIncrease, 50)
+    if (isAgedBrie(item)) {
+      val qualityIncrease = if (item.sellIn <= 0) 2 else 1
+      item.quality = Math.min(item.quality + qualityIncrease, 50)
     }
-    if (isBackstagePasses(getItem)) {
-      val qualityIncrease = if (getItem.sellIn >= 11) 1 else if (getItem.sellIn >= 6) 2 else 3
-      getItem.quality = Math.min(getItem.quality + qualityIncrease, 50)
-      if (getItem.sellIn == 0) getItem.quality = 0
+    if (isBackstagePasses(item)) {
+      val qualityIncrease = if (item.sellIn >= 11) 1 else if (item.sellIn >= 6) 2 else 3
+      item.quality = Math.min(item.quality + qualityIncrease, 50)
+      if (item.sellIn == 0) item.quality = 0
     }
   }
 
