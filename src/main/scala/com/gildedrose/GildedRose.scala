9798158da41b3d369194d5a03fce19025097e6cf
Matthias Hansen <matthias.hansen@rwth-aachen.de>
2017-04-11 16:53:32 +0200

    make each body smaller

--- src/main/scala/com/gildedrose/GildedRose.scala
+++ src/main/scala/com/gildedrose/GildedRose.scala
@@ -27,19 +27,17 @@ class GildedRose(val items: Array[Item]) {
   }
 
   private def determineNewQuality(item: Item) = {
-
+    var qualityIncrease
     if (shouldJustLoseQuality(item)) {
-      val qualityIncrease = -(if (item.sellIn <= 0) 2 else 1)
-      item.quality = Math.max(item.quality + qualityIncrease, 0)
+      qualityIncrease = -(if (item.sellIn <= 0) 2 else 1)
     }
     if (isAgedBrie(item)) {
-      val qualityIncrease = if (item.sellIn <= 0) 2 else 1
-      item.quality = Math.min(item.quality + qualityIncrease, 50)
+      qualityIncrease = if (item.sellIn <= 0) 2 else 1
     }
     if (isBackstagePasses(item)) {
-      val qualityIncrease = if (item.sellIn >= 11) 1 else if (item.sellIn >= 6) 2 else 3
-      item.quality = Math.min(item.quality + qualityIncrease, 50)
+      qualityIncrease = if (item.sellIn >= 11) 1 else if (item.sellIn >= 6) 2 else 3
     }
+    item.quality = Math.min(Math.max(item.quality + qualityIncrease, 0),50)
     if(isBackstagePasses(item) && item.sellIn == 0) item.quality = 0
   }
 
