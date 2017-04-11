0f937e12a58001263f86e18fe1724e647b589d5c
Matthias Hansen <matthias.hansen@rwth-aachen.de>
2017-04-11 16:36:35 +0200

    auto extract methods for string comparisons

--- src/main/scala/com/gildedrose/GildedRose.scala
+++ src/main/scala/com/gildedrose/GildedRose.scala
@@ -3,21 +3,34 @@ package com.gildedrose
 class GildedRose(val items: Array[Item]) {
 
   def shouldJustLoseQuality(item:Item) =
-    !item.name.equals("Aged Brie") &&
-      !item.name.equals("Backstage passes to a TAFKAL80ETC concert") &&
-      !item.name.equals("Sulfuras, Hand of Ragnaros") &&
+    !isAgedBrie(item) &&
+      !isBackstagePasses(item) &&
+      !isSulfuras(item) &&
       item.quality > 0
+
+  private def isSulfuras(item: Item) = {
+    item.name.equals("Sulfuras, Hand of Ragnaros")
+  }
+
+  private def isBackstagePasses(item: Item) = {
+    item.name.equals("Backstage passes to a TAFKAL80ETC concert")
+  }
+
+  private def isAgedBrie(item: Item) = {
+    item.name.equals("Aged Brie")
+  }
+
   def updateQuality() {
     for (i <- 0 until items.length) {
       if (shouldJustLoseQuality(items(i))) {
         val qualityLoss = if(items(i).sellIn <= 0) 2 else 1
         items(i).quality = Math.max(items(i).quality - qualityLoss,0)
       }
-      if(items(i).name.equals("Aged Brie")) {
+      if(isAgedBrie(items(i))) {
         val qualityIncrease = if(items(i).sellIn <= 0) 2 else 1
         items(i).quality = Math.min(items(i).quality + qualityIncrease, 50)
       }
-      if (items(i).name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+      if (isBackstagePasses(items(i))) {
         val qualityIncrease = if(items(i).sellIn >= 11) 1 else if(items(i).sellIn >= 6) 2 else 3
         items(i).quality = Math.min(items(i).quality + qualityIncrease,50)
         if(items(i).sellIn == 0) items(i).quality = 0
@@ -30,6 +43,6 @@ class GildedRose(val items: Array[Item]) {
   }
 
   private def hasToBeSold(i: Item) = {
-    !i.name.equals("Sulfuras, Hand of Ragnaros")
+    !isSulfuras(i)
   }
 }
