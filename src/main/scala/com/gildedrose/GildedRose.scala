8d9780f48c3a5e84650161003be51caf013b421c
Matthias Hansen <matthias.hansen@rwth-aachen.de>
2017-04-11 17:42:10 +0200

    refactor quality updating

--- src/main/scala/com/gildedrose/GildedRose.scala
+++ src/main/scala/com/gildedrose/GildedRose.scala
@@ -16,23 +16,22 @@ class GildedRose(val items: Array[Item]) {
   private def updatedSellDeadline(item: Item) = if (hasToBeSold(item)) item.sellIn - 1 else item.sellIn
 
   private def updatedQuality(item: Item):Int = {
-    val qualityIncrease =
-      if(isSulfuras(item)) qualityChangeSulfuras(item)
-      else if (isAgedBrie(item)) qualityChangeAgedBrie(item)
-      else if (isBackstagePasses(item)) qualityChangeBackstagePasses(item)
-      else qualityChangeNormalItem(item)
-    if(qualityIncrease == 0) item.quality else withinQualityBounds(item.quality + qualityIncrease)
+      if(isSulfuras(item)) newQualitySulfuras(item)
+      else if (isAgedBrie(item)) newQualityAgedBrie(item)
+      else if (isBackstagePasses(item)) newQualityBackstagePasses(item)
+      else newQualityNormalItem(item)
   }
 
-  private def qualityChangeBackstagePasses(item: Item) =
+  private def newQualityBackstagePasses(item: Item) = withinQualityBounds(item.quality + qualityChangeBackstagePasses(item))
+  private def newQualityNormalItem(item: Item) = withinQualityBounds(item.quality + (if (item.sellIn <= 0) -2 else -1))
+  private def newQualityAgedBrie(item: Item)   = withinQualityBounds(item.quality + (if (item.sellIn <= 0)  2 else  1))
+  private def newQualitySulfuras(item:Item)    = item.quality
+
+  private def qualityChangeBackstagePasses(item:Item)=
     if (item.sellIn >= 11) 1
     else if (item.sellIn >= 6) 2
     else if (item.sellIn > 0) 3
     else -item.quality
-
-  private def qualityChangeNormalItem(item: Item) = if (item.sellIn <= 0) -2 else -1
-  private def qualityChangeAgedBrie(item: Item) = -qualityChangeNormalItem(item)
-  private def qualityChangeSulfuras(item:Item) = 0
 
   private def withinQualityBounds(oldQuality:Int) = pullIntoBounds(0,50)(oldQuality)
   private def pullIntoBounds(lower:Int,upper:Int) = (x:Int) => Math.min(Math.max(x, lower), upper)
